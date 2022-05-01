2904b05668092635c348c9a4a9d6287fcc0f4abe
jonatan52 <tarriba52@gmail.com>
2022-05-01 17:34:08 -0500

    Ejercicios 1 y 2 sesion dos

---
+++ Ejercicio 1/ejercicio_1.py
@@ -0,0 +1,6 @@
+# Ejercicio 1 sesion 2
+# Autor: jonatan tarriba
+
+cadena_texto = "Nunca pares de aprender"
+print(type(cadena_texto))
+print(cadena_texto)
---
+++ Ejercicio 2/ejercicio_2.py
@@ -0,0 +1,11 @@
+# Ejercicio 2 sesion 2
+# Autor: jonatan tarriba
+
+cadena_texto = "Nunca pares de aprender"
+print(type(cadena_texto))
+print(cadena_texto)
+print()
+
+cadena_texto = "se ha modificado la frase"
+print(cadena_texto)
+
